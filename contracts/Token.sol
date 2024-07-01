79cb04c692db555ffdbdad73c624fd9d1846a63f
Saim Can Özgen <sco11@protonmail.com>
2024-07-01 12:24:51 +0300

    Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+
 contract canToken is ERC20 {
     constructor() ERC20("Can Token", "CAN") {
-        _mint(msg.sender, 1773000 * 10**decimals());
+        _mint(msg.sender, 1773000 * 10 ** decimals());
     }
-} 
+}
